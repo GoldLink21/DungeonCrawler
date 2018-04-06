75b74ab0139dd362e807f2bbfbef54eb99c42149
GoldLink21 <30599735+GoldLink21@users.noreply.github.com>
2018-04-06 13:58:54 -0400

    Initial commit

--- src/SoundLoader.java
+++ src/SoundLoader.java
@@ -1,3 +1,20 @@
-public SoundLoader{
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 
+public class SoundLoader{
+    
+    public static void playSound(String fileName){
+      Clip clip = null;
+      try{
+        AudioInputStream audioIn = AudioSystem.getAudioInputStream( getClass().getResource( filename ) );
+        clip = AudioSystem.getClip();
+        clip.open( audioIn );
+      }catch( Exception e ){e.printStackTrace();}
+      
+      if( clip.isRunning() ) clip.stop();
+            clip.setFramePosition( 0 );
+      clip.start();
+    }
+  
 }
