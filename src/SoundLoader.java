e23324c17858eb66f7a20bcdc02126bf80451881
Chris <holtschr000@warren.k12.in.us>
2018-04-11 16:17:56 -0400

    Fixed Sound loader

--- src/SoundLoader.java
+++ src/SoundLoader.java
@@ -3,27 +3,14 @@ import java.io.File;
 import java.io.IOException;
 
 public class SoundLoader{
-
-    public SoundLoader(String fileName){play(fileName);}
-
-    //I stole most of these methods, just made them more efficient
-    private boolean completed;
-
-    public void play(String audioFilePath){
+    public SoundLoader(final String fileName){
         try{
-            AudioInputStream audioStream = AudioSystem.getAudioInputStream(new File(audioFilePath));
-            AudioFormat format = audioStream.getFormat();
-            Clip audioClip = (Clip) AudioSystem.getLine(new DataLine.Info(Clip.class, format));
-
-            //I figured out how to use a lambda!
-            audioClip.addLineListener(e->{if(e.getType()==LineEvent.Type.STOP)completed = true;});
-
-            audioClip.open(audioStream);
-            audioClip.start();
-            while(!completed)try{Thread.sleep(0);}catch(InterruptedException ex){ex.printStackTrace();}
-            audioClip.close();
-        }catch(UnsupportedAudioFileException ex){ex.printStackTrace();
-        }catch(LineUnavailableException ex){ex.printStackTrace();
-        }catch(IOException ex){ex.printStackTrace();}
+            AudioInputStream audioIn = AudioSystem.getAudioInputStream(new File(fileName));
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioIn);
+            clip.start();
+        }catch(UnsupportedAudioFileException e){e.printStackTrace();
+        }catch(IOException e){e.printStackTrace();
+        }catch(LineUnavailableException e){e.printStackTrace();}
     }
 }
